2a163144b6f7f7896f09384aafe900df55b1382c
Dillan <dillansdev@gmail.com>
2024-05-30 21:49:24 -0500

    Add and configure CORS support

--- app/__init__.py
+++ app/__init__.py
@@ -6,9 +6,11 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
+    # Habilita CORS
+    CORS(app, resources={r"/*": {"origins": "*"}})
+
     with app.app_context():
         # Importa las rutas
-        CORS(app, origins="*")
         from .routes import main_bp
         app.register_blueprint(main_bp)
 
